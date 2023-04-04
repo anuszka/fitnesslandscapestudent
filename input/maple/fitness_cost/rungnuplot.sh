88135aeae6298fbe8c8f8ab75673486ed4275687
Anna Ochab-Marcinek <anna.ochab.marcinek@gmail.com>
2023-04-04 22:37:55 +0200

    Add help

--- input/maple/fitness_cost/rungnuplot.sh
+++ input/maple/fitness_cost/rungnuplot.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+display_help() {
+    # taken from https://stackoverflow.com/questions/55170843/gnuplot-buttons-greyed-out
+    echo "Usage: `basename $0` gnuplot_script_file"
+    echo
+    echo "   Script location: ~/myscripts/"
+    exit 1
+}
+
+
 ### Preserves the buttons
 ### Usage: ./rungnuplot.sh <gnuplot script file>
 
