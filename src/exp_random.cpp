9cb2502de052b6aeb031d2310bd5e1122d076acc
Anna Ochab-Marcinek <anna.ochab.marcinek@gmail.com>
2023-04-14 16:31:05 +0200

    Should be unnecessary

--- src/exp_random.cpp
+++ src/exp_random.cpp
@@ -1,18 +1,19 @@
-#include"include/exp_random.h"
+#include "include/exp_random.h"
 
-std::map<double, int> expRng(double exp_dist_mean, int n ){
+std::map<double, int> expRng(double exp_dist_mean, int n)
+{
 
     double const exp_dist_lambda = 1 / exp_dist_mean;
 
     std::random_device rd;
-    std::exponential_distribution<> rng (exp_dist_lambda);
+    std::exponential_distribution<> rng(exp_dist_lambda);
     std::mt19937 rnd_gen(rd());
 
     std::map<double, int> result_set;
 
-    for(int i = 0; i < n; i++)
-        ++result_set[(rng (rnd_gen) * 2)];
+    for (int i = 0; i < n; i++)
+        ++result_set[(rng(rnd_gen) * 2)];
 
     return result_set;
-
 }
+
