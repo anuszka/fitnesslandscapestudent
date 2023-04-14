5040e57ce16e1a478c0c3d4fd52dd566a1dfc3a3
Anna Ochab-Marcinek <anna.ochab.marcinek@gmail.com>
2023-04-14 16:32:51 +0200

    My exp times

--- src/include/exp_random.h
+++ src/include/exp_random.h
@@ -2,6 +2,7 @@
 #include<random>
 #include<iomanip>
 #include<map>
+#include<stack>
 #include<iterator>
 
 
@@ -9,6 +10,6 @@
 #define EXP_RANDOM_H
 
 std::map<double, int> expRng(double exp_dist_mean, int n );
-
+std::stack<double> exp_random_numbers(double exp_dist_mean, int n );
 
 #endif
